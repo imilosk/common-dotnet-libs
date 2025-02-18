b0de27d1a033c4711f9ee31e0fcb167b57e8c877
miloshkostadinovski <milos.kostadinoski97@gmail.com>
2025-02-18 17:21:03 +0100

    Update IMilosk.Messaging.RabbitMq

--- IMilosk.Messaging.RabbitMq/Constants/RabbitMqHeaderNames.cs
+++ IMilosk.Messaging.RabbitMq/Constants/RabbitMqHeaderNames.cs
@@ -4,4 +4,7 @@ public static class RabbitMqHeaderNames
 {
     public const string ContentType = "content-type";
     public const string UserAgent = "user-agent";
+    public const string XMessageTtl = "x-message-ttl";
+    public const string XDeadLetterExchange = "x-dead-letter-exchange";
+    public const string XDeadLetterRoutingKey = "x-dead-letter-routing-key";
 }
